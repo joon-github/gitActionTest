24194926091b37c46ae2da07984df9fb77098805
beomJoon <104752645+joon-github@users.noreply.github.com>
2022-10-12 15:27:23 +0900

    test3

--- my-agora-states-client/src/App.js
+++ my-agora-states-client/src/App.js
@@ -8,7 +8,7 @@ function App() {
   useEffect(() => {
     getDiscussion();
   }, []);
-
+const a ="A"
   const getDiscussion = () => {
     return fetch(domain + "/discussions")
       .then((res) => res.json())
